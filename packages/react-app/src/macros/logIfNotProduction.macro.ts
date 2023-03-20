59fe7e8bfb0b211c4f0ec44fac9a737965fdb602
Ryan Berckmans <ryanberckmans@gmail.com>
2023-03-20 10:53:20 -0500

    Add ts-macros and logIfNotProduction macro
    Note that ts-macros requires compiler support, which I wasn't yet able
    to get working with create-react-app. So macros do not yet work.
    
    Howeve, the ts-macros author helpfully created an example of how to get
    it working in Vite, so we can use this to get macros working after we
    switch to Vite.
    https://github.com/GoogleFeud/ts-macros-vite-example/tree/main/src

---
+++ packages/react-app/src/macros/logIfNotProduction.macro.ts
@@ -0,0 +1,38 @@
+// This file is an example macro to log to console when not in
+// production, and for the log statemente to not exist in production.
+// It allows you to do things like `$logIfNotProduction!('config
+// loaded', config)` and have that macro expand to console.log('config
+// loaded', config) or be removed entirely in production. However, for
+// this to work, ts-macros needs compiler support, and the typescript
+// compiler doesn't allow custom transformers. There are two
+// recommended options: using a fork of the typescript compiler
+// `ttypescript` that does support custom transformers, which I don't
+// want to do; or supporting a TsMacro transformer as part of the
+// compiler process, which I couldn't figure out how to make work with
+// create-react-app.
+// TODO see if I can get ts-macros working after we switch to Vite.
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck TODO once the ts-macro transformer is correctly being run, this file may typecheck normally and @ts-nocheck might be able to be removed
+import createMacro from 'ts-macros';
+
+export const $logIfNotProduction = createMacro(logIfNotProductionImpl);
+
+function logIfNotProductionImpl({ references, babel }) {
+  const isProduction: boolean = process.env['REACT_APP_IS_PRODUCTION'] === 'true'; // WARNING this boolean definition has been copied from isProduction.ts because we're unable to import a non-macro module from inside a macro module (as the non-macro module doesn't exist yet)
+  if (isProduction) return;
+  const { types: t } = babel;
+  references.default.forEach(referencePath => {
+    if (t.isCallExpression(referencePath.parent)) {
+      referencePath.parentPath.replaceWith(
+        t.expressionStatement(
+          t.callExpression(
+            t.memberExpression(t.identifier('console'),
+            t.identifier('log')),
+            [...referencePath.parent.arguments],
+          )
+        )
+      );
+    }
+  });
+}
