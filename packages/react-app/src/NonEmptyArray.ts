963567b7655e186402500e861b0b4d2bf027ccc3
Ryan Berckmans <ryanberckmans@gmail.com>
2023-03-20 08:47:37 -0500

    Add isNonEmptyArray

--- packages/react-app/src/NonEmptyArray.ts
+++ packages/react-app/src/NonEmptyArray.ts
@@ -1,3 +1,7 @@
 
 // NonEmptyArray is a TypeScript type for a non-empty array
 export type NonEmptyArray<T> = [T, ...T[]];
+
+export function isNonEmptyArray<T>(ts: T[]): ts is NonEmptyArray<T> {
+  return ts.length > 0;
+}
