f6f67571e9eb27836071bd90b1cacc098a1b4ff2
Ryan Berckmans <ryanberckmans@gmail.com>
2023-03-20 06:41:42 -0500

    Add animate-spin-spin custom tailwind class

--- packages/react-app/tailwind.config.js
+++ packages/react-app/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-fast': 'spin 0.618s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
