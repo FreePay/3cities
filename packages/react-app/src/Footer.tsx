3f55f2352b968c4e567535737b3a4f68390fc1a0
Ryan Berckmans <ryanberckmans@gmail.com>
2023-03-27 13:47:21 -0500

    Ensure app height is 100% of viewport and grows with content

--- packages/react-app/src/Footer.tsx
+++ packages/react-app/src/Footer.tsx
@@ -4,32 +4,30 @@ import { Link } from "react-router-dom";
 
 export function Footer() {
   return (
-    <footer className="flex h-[76px] items-center bg-white p-5 text-sm font-medium">
-      <div className="mx-auto w-full max-w-screen-lg">
-        <div className="flex items-center gap-2.5">
-          <Link
-            to="/"
-            className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition hover:bg-black hover:bg-opacity-10"
-          >
-            <FaHome />
-            Home
-          </Link>
-          {/* <Link
+    <footer className="flex items-center bg-white p-5 text-sm font-medium w-full justify-center">
+      <div className="flex items-center gap-2.5">
+        <Link
+          to="/"
+          className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition hover:bg-black hover:bg-opacity-10"
+        >
+          <FaHome />
+          Home
+        </Link>
+        {/* <Link
             to="/"
             className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition hover:bg-black hover:bg-opacity-10"
           >
             <FaQuestionCircle />
             FAQ
           </Link> */}
-          <a href="https://twitter.com/3cities_xyz" target="_blank" rel="noreferrer" className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition hover:bg-black hover:bg-opacity-10">
-            <FaTwitter />
-            Twitter
-          </a>
-          <a href="https://twitter.com/3cities_xyz" target="_blank" rel="noreferrer" className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition hover:bg-black hover:bg-opacity-10">
-            <FaCommentAlt />
-            DM Feedback
-          </a>
-        </div>
+        <a href="https://twitter.com/3cities_xyz" target="_blank" rel="noreferrer" className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition hover:bg-black hover:bg-opacity-10">
+          <FaTwitter />
+          Twitter
+        </a>
+        <a href="https://twitter.com/3cities_xyz" target="_blank" rel="noreferrer" className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition hover:bg-black hover:bg-opacity-10">
+          <FaCommentAlt />
+          DM Feedback
+        </a>
       </div>
     </footer>
   );
