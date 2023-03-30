fc53f1efe4d4596ad25f86b1739d05b5b55ee63d
Ryan Berckmans <ryanberckmans@gmail.com>
2023-03-30 14:58:45 -0500

    Update refresh strategy in token balance fetcher
    See notes on useIsEnabledSmartRefresh and `enabled`.

--- packages/react-app/src/hooks/useMemoTokenBalance.ts
+++ packages/react-app/src/hooks/useMemoTokenBalance.ts
@@ -1,18 +1,31 @@
 import { BigNumber } from "@ethersproject/bignumber";
-import { useTokenBalance } from "@usedapp/core";
 import { useMemo } from "react";
+import { useBalance } from 'wagmi';
+import { makeAddress } from "../makeAddress";
+import { useIsEnabledSmartRefresh } from "../useIsEnabledSmartRefresh";
 
-// useMemoTokenBalance is a React hook that returns a live-reloaded
-// token balance for the passed token address, address, and chainId.
-// useMemoTokenBalance exists to fix a memoization bug in usedapp's
-// useTokenBalance, see below.
-export function useMemoTokenBalance(
-  tokenAddress: string, // token contract address whose token balance will be live-reloaded
+// useLiveTokenBalance is a React hook that returns a live-reloaded token
+// balance for the passed token contract address, address, and chainId.
+export function useLiveTokenBalance(
+  tokenContractAddress: string, // token contract address whose token balance will be live-reloaded
   address: string, // address whose token balance will be live-reloaded
   chainId: number, // chainId on which tokenAddress contract resides
 ): BigNumber | undefined {
-  const bRaw = useTokenBalance(tokenAddress, address, { chainId });
+  const enabled = useIsEnabledSmartRefresh();
+  const args = useMemo(() => {
+    return {
+      enabled, // here, we are periodically toggling enabled to force a useBalance refresh. We explored solutions like `watch: true` and `staleTime/cacheTime`, and none of them worked for us. See 'redesign of live balance fetching' in 3cities design. Crucially, this toggle works because when useBalance is disabled, it continues returning the most recent result and doesn't return undefined, so the client is unaware that useBalance has been disabled.
+      token: makeAddress(tokenContractAddress),
+      address: makeAddress(address),
+      chainId,
+      onError(error: Error) {
+        console.error('useMemoTokenBalance: error fetching balance with wagmi.useBalance, chainId', chainId, 'token', tokenContractAddress, 'address', address, 'error', error);
+      },
+    };
+  }, [chainId, address, tokenContractAddress, enabled]);
+  const r = useBalance(args);
+  const bRaw = r.data === undefined ? undefined : r.data.value;
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const b = useMemo(() => bRaw, [bRaw && bRaw._hex]); // there's a performance bug in useDapp where their hook useTokenBalance delegates to useCall which delegates to useCalls which makes use of useMemo on call results as a general solution to avoid triggering re-renders https://github.com/TrueFiEng/useDApp/blob/40c1d86d2516a948fc88bf52ceed457dac22345a/packages/core/src/hooks/useCall.ts#L52, however, two BigNumbers for the same value fail this memoization check because they are different BigNumber objects, and this causes useTokenBalance to trigger re-renders every new block because the BigNumber object instance has changed even if the balance has not changed. To fix this performance bug of triggering an unnecessary re-render every new block even when balance doesn't change, we memoize the BigNumber by its hex representation
+  const b = useMemo(() => bRaw, [bRaw && bRaw._hex]); // ensure memoization of underlying balance as the BigNumber object instance may change when toggling enabled/disabled of useBalance
   return b;
 }
