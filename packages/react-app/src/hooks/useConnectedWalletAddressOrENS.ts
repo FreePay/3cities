cc0b5dc6d82d268d96ddaa868989987ba1d9b1f5
Ryan Berckmans <ryanberckmans@gmail.com>
2023-03-30 16:42:28 -0500

    Rebuild useConnectedWalletAddressOrENS on wagmi
    - no more usedapp
    - memoize args
    - always use the L1 or gorli for ENS lookup

--- packages/react-app/src/hooks/useConnectedWalletAddressOrENS.ts
+++ packages/react-app/src/hooks/useConnectedWalletAddressOrENS.ts
@@ -1,12 +1,24 @@
-import { useEthers, useLookupAddress } from "@usedapp/core";
+import { useMemo } from "react";
+import { goerli, mainnet, useAccount, useEnsName } from 'wagmi';
+import { isProduction } from "../isProduction";
 
-// useConnectedWalletAddressOrENS returns the currently connected wallet address's
-// primary ENS name, or else the address itself if no primary ENS name
-// is set, or else undefined if no address is connected.
+// useConnectedWalletAddressOrENS returns the currently connected
+// wallet address's primary ENS name, or else the address itself if no
+// primary ENS name is set, or else undefined if no wallet is
+// connected.
 export default function useConnectedWalletAddressOrENS(): string | undefined {
-    const { account } = useEthers();
-    const ens = useLookupAddress(account);
-    if (typeof account !== 'string') return undefined;
-    if (typeof ens === 'string') return ens;
-    return account;
+    const { address } = useAccount();
+    const args = useMemo(() => {
+        if (address !== undefined) return {
+            chainId: isProduction ? mainnet.id : goerli.id,
+            address,
+        };
+        else return {
+            enabled: false,
+        };
+    }, [address]);
+    const { data: ensName } = useEnsName(args);
+    if (ensName !== undefined && ensName !== null) return ensName;
+    else if (address !== undefined) return address;
+    else return undefined;
 }
