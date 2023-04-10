299a6c30e7433163127ab026a6101d12249c8c1a
Ryan Berckmans <ryanberckmans@gmail.com>
2023-04-10 13:28:23 -0500

    Add initial react-router v6 routes using <Outlet>
    Now using a "data router" to enable new v6 APIs.
    
    New GlobalErrorBoundary catches errors in the entire app, including
    routing errors such as URL not found.
    
    react-router supports a loading placeholder to display when the initial
    routes are being rendered. We use a loading placeholder that's visually
    identical to the static loading placeholder in public.html so that a
    user that sees both placeholders will not experience jank when
    react-router initializes.
    
    The new routing topology is as follows
    
    GlobalProviders // all global providers at root, eg. wagmiClient
      MainWrapper // ancestor of all routes with canonical header/footer.
        <app routes, eg. Home screen, Request Money flow>
      ConversionWrapper // ancestor of all routes optimized to convert.
        // ContextLink is the ancestor of all routes that require a
        // deserialized context link, eg. a checkout:
        ContextLink
          <routes using both conversion wrapper and context link, eg. pay>
    
    Most product routes aren't implemented yet.

---
+++ packages/react-app/src/GlobalProviders.tsx
@@ -0,0 +1,32 @@
+import { ConnectKitProvider } from "connectkit";
+import { ConnectKitOptions } from "connectkit/build/types";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Toaster } from 'sonner';
+import { WagmiConfig } from "wagmi";
+import { ConnectedWalletAddressContextObserverProvider } from "./connectedWalletContextProvider";
+import "./index.css";
+import { wagmiClient } from "./wagmiClient";
+
+const connectKitOptions: ConnectKitOptions = {
+  walletConnectName: "WalletConnect", // default is "Other Wallets" which I find confusing because anybody who knows they can scan a qr code from mobile will most likely be looking for the name "WalletConnect"
+  bufferPolyfill: false, // disable connectkit's Buffer polyfill because we have our own
+  initialChainId: 0, // don't target a specific chain to connect to
+  enforceSupportedChains: false,
+  hideNoWalletCTA: true, // hide "I don't have a wallet" in the connectkit modal because 3cities isn't in the business of converting users into non-custodial wallets; we serve two segments: crypto natives that already have wallets, and sellers new to crypto for which we'll offer email/sms oauth options to create a new semi-custodial wallet
+  hideQuestionMarkCTA: true, // true for same reason as hideNoWalletCTA
+};
+
+export const GlobalProviders = () => {
+  return <WagmiConfig client={wagmiClient}>
+    {/* <ConnectWalletProvider chains={chainsSupportedBy3cities}>  TODO connect-wallet support blocked by runtime error https://github.com/Shopify/blockchain-components/issues/16 */}
+    <ConnectKitProvider options={connectKitOptions}>
+      <ConnectedWalletAddressContextObserverProvider>
+        <Toaster /> {/* NB here we put the toaster inside the wagmi, connectkit, and addressContext providers so that the toast clients can have access to these services */}
+        <Outlet />
+      </ConnectedWalletAddressContextObserverProvider>
+    </ConnectKitProvider>
+    {/* </ConnectWalletProvider> */}
+    {/* <Web3ModalInstance /> */ /* TODO we can't use web3modal right now because of bugs in WalletConnectConnector which should become resolved after these libs finish the current transition to walletconnect v2. See notes on WalletConnectConnector in wagmi config. */}
+  </WagmiConfig>;
+};
