304612bf56e0e9761883e9584dc4783c34edc0f8
Ryan Berckmans <ryanberckmans@gmail.com>
2023-04-11 15:47:13 -0500

    Rebuild useConnectedWalletAddressOrENS on useAddressOrENS

--- packages/react-app/src/hooks/useConnectedWalletAddressOrENS.ts
+++ packages/react-app/src/hooks/useConnectedWalletAddressOrENS.ts
@@ -1,24 +1,13 @@
-import { useMemo } from "react";
-import { goerli, mainnet, useAccount, useEnsName } from 'wagmi';
-import { isProduction } from "../isProduction";
+import { useAccount } from 'wagmi';
+import { useAddressOrENS } from '../useAddressOrENS';
 
-// useConnectedWalletAddressOrENS returns the currently connected
-// wallet address's primary ENS name, or else the address itself if no
-// primary ENS name is set, or else undefined if no wallet is
-// connected.
-export default function useConnectedWalletAddressOrENS(): string | undefined {
+type Opts = NonNullable<Parameters<typeof useAddressOrENS>[1]>;
+
+// useConnectedWalletAddressOrENS returns the connected wallet's primary
+// ENS name, or else the address itself if no primary ENS name is set, and
+// truncates the returned ENS name or address if the passed truncated
+// option is set.
+export function useConnectedWalletAddressOrENS(opts?: Opts): string | undefined {
     const { address } = useAccount();
-    const args = useMemo(() => {
-        if (address !== undefined) return {
-            chainId: isProduction ? mainnet.id : goerli.id,
-            address,
-        };
-        else return {
-            enabled: false,
-        };
-    }, [address]);
-    const { data: ensName } = useEnsName(args);
-    if (ensName !== undefined && ensName !== null) return ensName;
-    else if (address !== undefined) return address;
-    else return undefined;
+    return useAddressOrENS(address, opts);
 }
