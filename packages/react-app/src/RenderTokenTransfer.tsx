520fff49f812eaa6edaaec9e764f486c22aa2c88
Ryan Berckmans <ryanberckmans@gmail.com>
2023-04-11 15:54:37 -0500

    fixup

--- packages/react-app/src/RenderTokenTransfer.tsx
+++ packages/react-app/src/RenderTokenTransfer.tsx
@@ -14,7 +14,8 @@ type RenderTokenTransferProps = {
 // the passed strategy.
 export const RenderTokenTransfer: React.FC<RenderTokenTransferProps> = ({ tt }) => {
   const t = tt.token;
-  return <span>Pay {formatFloat(formatUnits(tt.amountAsBigNumberHexString, t.decimals), getDecimalsToRenderForTokenTicker(t.ticker))} {t.ticker} on {getSupportedChainName(t.chainId)
+  // TODO it'd be nice to show the token image instead of the ticker (eg. USDC icon instead of "USDC"), and the same for the chain --> some apps have adopted the convention of showing a little chain icon in the top left of the token icon... maybe I should do that? where's the open-source code and assets to do so?
+  return <span>{formatFloat(formatUnits(tt.amountAsBigNumberHexString, t.decimals), getDecimalsToRenderForTokenTicker(t.ticker))} {t.ticker} on {getSupportedChainName(t.chainId)
   }</span>
 }
 
