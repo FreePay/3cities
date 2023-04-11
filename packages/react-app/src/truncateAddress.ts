e7415260848fd03edd63d53efa8bc75aec6f9639
Ryan Berckmans <ryanberckmans@gmail.com>
2023-04-11 15:49:41 -0500

    Add trunucateEthAddress, truncateENSAddress

---
+++ packages/react-app/src/truncateAddress.ts
@@ -0,0 +1,21 @@
+// from https://github.com/family/connectkit/blob/main/packages/connectkit/src/utils/index.ts
+
+const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/;
+
+// truncateEthAddress truncates the passed Ethereum address.
+export const truncateEthAddress = (address?: string, separator: string = '••••') => {
+  if (!address) return '';
+  const match = address.match(truncateRegex);
+  if (!match) return address;
+  return `${match[1]}${separator}${match[2]}`;
+};
+
+// truncateENSAddress truncates the passed ENS name to be no longer than
+// the passed maxLength.
+export const truncateENSAddress = (ensName: string, maxLength: number) => {
+  if (ensName.length > maxLength) {
+    return ensName.replace('.eth', '').slice(0, maxLength) + '...';
+  } else {
+    return ensName;
+  }
+};
