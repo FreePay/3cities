5ee857a9786c35a9b8c24f31e3d17db3a4c448d6
Ryan Berckmans <ryanberckmans@gmail.com>
2023-04-11 15:53:40 -0500

    Add opts.showAllZeroesAfterDecimal to RenderLogicalAssetAmount

--- packages/react-app/src/RenderLogicalAssetAmount.tsx
+++ packages/react-app/src/RenderLogicalAssetAmount.tsx
@@ -1,16 +1,17 @@
 import { formatUnits } from "@ethersproject/units";
 import React from "react";
-import { formatFloat } from "./formatFloat";
+import { formatFloat, FormatFloatOpts } from "./formatFloat";
 import { getDecimalsToRenderForLogicalAssetTicker, logicalAssetDecimals, LogicalAssetTicker } from "./logicalAssets";
 
 type RenderLogicalAssetAmountProps = {
   logicalAssetTicker: LogicalAssetTicker; // logical asset ticker of the amount to be rendered
   amountAsBigNumberHexString: string; // logical asset amount to render as a BigNumber.toHexString(). Note that this amount must have been constructed using parseLogicalAssetAmount to properly respect logical asset decimal count
+  showAllZeroesAfterDecimal?: true; // iff true, a rendered amount ending in all zeroes after the decimal point will retain the zeroes (eg. "1.00"), otherwise they are truncated by default (eg. 1.00 -> "1")
 }
 
 // RenderLogicalAssetAmount is a referentially transparent component
 // to render the passed logical asset balance.
-export const RenderLogicalAssetAmount: React.FC<RenderLogicalAssetAmountProps> = ({ logicalAssetTicker, amountAsBigNumberHexString }) => {
+export const RenderLogicalAssetAmount: React.FC<RenderLogicalAssetAmountProps> = ({ logicalAssetTicker, amountAsBigNumberHexString, showAllZeroesAfterDecimal }) => {
   const prefix: string = (() => {
     switch (logicalAssetTicker) {
       case 'ETH': return '';
@@ -29,5 +30,7 @@ export const RenderLogicalAssetAmount: React.FC<RenderLogicalAssetAmountProps> =
     }
   })();
 
-  return <span>{prefix}{formatFloat(formatUnits(amountAsBigNumberHexString, logicalAssetDecimals), getDecimalsToRenderForLogicalAssetTicker(logicalAssetTicker))}{suffix}</span>;
+  const formatFloatOpts: FormatFloatOpts | undefined = showAllZeroesAfterDecimal ? { showAllZeroesAfterDecimal: true } : undefined;
+  const formattedFloat = formatFloat(formatUnits(amountAsBigNumberHexString, logicalAssetDecimals), getDecimalsToRenderForLogicalAssetTicker(logicalAssetTicker), formatFloatOpts);
+  return <span>{prefix}{formattedFloat}{suffix}</span>;
 }
