5d3dd926d5b156a321441a476aa55e4cc9331a90
Ryan Berckmans <ryanberckmans@gmail.com>
2023-04-11 15:55:12 -0500

    fixup

--- packages/react-app/src/ContentWrapper.tsx
+++ packages/react-app/src/ContentWrapper.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 type Props = {
-  heading: React.ReactNode
+  heading?: React.ReactNode
   children?: React.ReactNode
 };
 
 export const ContentWrapper: React.FC<Props> = ({ heading, children }) => {
   return (
     <>
-      <span className="flex justify-center pb-5 text-3xl font-bold tracking-tight text-white">
+      {heading !== undefined && <span className="flex justify-center pb-5 text-3xl font-bold tracking-tight text-white">
         {heading}
-      </span>
+      </span>}
       <div className="w-full max-w-2xl overflow-hidden rounded-lg bg-white p-5 shadow-lg">
         {children}
       </div>
