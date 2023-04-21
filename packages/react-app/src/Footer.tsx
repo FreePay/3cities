d322345cdfa283b115ec4e6bdc018835474b283c
Ryan Berckmans <ryanberckmans@gmail.com>
2023-04-21 08:47:15 -0500

    fixup

--- packages/react-app/src/Footer.tsx
+++ packages/react-app/src/Footer.tsx
@@ -8,23 +8,23 @@ export function Footer() {
       <div className="flex items-center gap-2.5">
         <Link
           to="/"
-          className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition hover:bg-black hover:bg-opacity-10"
+          className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition sm:hover:bg-black sm:hover:bg-opacity-10"
         >
           <FaHome />
           Home
         </Link>
         {/* <Link
             to="/"
-            className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition hover:bg-black hover:bg-opacity-10"
+            className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition sm:hover:bg-black sm:hover:bg-opacity-10"
           >
             <FaQuestionCircle />
             FAQ
           </Link> */}
-        <a href="https://twitter.com/3cities_xyz" target="_blank" rel="noreferrer" className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition hover:bg-black hover:bg-opacity-10">
+        <a href="https://twitter.com/3cities_xyz" target="_blank" rel="noreferrer" className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition sm:hover:bg-black sm:hover:bg-opacity-10">
           <FaTwitter />
           Twitter
         </a>
-        <a href="https://twitter.com/3cities_xyz" target="_blank" rel="noreferrer" className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition hover:bg-black hover:bg-opacity-10">
+        <a href="https://twitter.com/3cities_xyz" target="_blank" rel="noreferrer" className="flex items-center gap-1 rounded-md px-2.5 py-1.5 transition sm:hover:bg-black sm:hover:bg-opacity-10">
           <FaCommentAlt />
           DM Feedback
         </a>
