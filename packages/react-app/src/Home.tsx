34f28f82c6c4fe8f4175fbaa62a9528efb1e595c
Ryan Berckmans <ryanberckmans@gmail.com>
2023-04-22 21:34:33 -0500

    Add About page content

--- packages/react-app/src/Home.tsx
+++ packages/react-app/src/Home.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { About } from "./About";
 
 export const Home: React.FC = () => {
   return <div>
+    <div className="sm:hidden">
+      <About />
+    </div>
   </div>;
 };
