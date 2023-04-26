04696c32ae5cfaa9208d485e24909cf84224a13a
Ryan Berckmans <ryanberckmans@gmail.com>
2023-04-26 16:21:20 -0500

    Simplify About page

--- packages/react-app/src/About.tsx
+++ packages/react-app/src/About.tsx
@@ -7,19 +7,19 @@ import { allTokenTickers } from "./tokens";
 export const About: React.FC = () => {
   return (<div className="mt-8 text-left max-w-2xl mx-auto">
     <h1 className="text-center text-3xl mb-4">About 3cities</h1>
-    <p className="text-xl mb-4">
-      3cities lets you request money from anybody in the world. It&apos;s fast and free.
+    <p className="mb-4">
+      Request money from anybody in the world by sending them a link.
     </p>
-    <p className="text-xl mb-4">
-       3cities is like Venmo or Cash App, but a website and no sign-up needed ever.
+    <p className="mb-4">
+      It&apos;s fast and free.
     </p>
-    <p className="text-xl mb-4">
-      Soon, you&apos;ll be able to use 3cities to request money with only an email address, phone number, or Facebook.
+    <p className="mb-4">
+      You need a crypto address or ENS name.
     </p>
-    <p className="text-xl mb-4">
-      But for the moment, you need a crypto address to request money (no need to connect wallet).
+    <p className="mb-4">
+      No sign-up and no connect wallet required.
     </p>
-    <h1 className="text-3xl mb-4 mt-16">3cities explanation for crypto experts</h1>
+    <h1 className="text-3xl mb-4 mt-48">3cities explanation for crypto experts</h1>
     <p className="text-xl mb-4">
       3cities is a payments app focused on accessibility and credible neutrality.
     </p>
