9448d055b5992c8434c58470f2395a08c681864a
Ryan Berckmans <ryanberckmans@gmail.com>
2023-04-25 21:12:25 -0500

    Add truncateEthAddressVeryShort
    For when very short addresses are needed.

--- packages/react-app/src/truncateAddress.ts
+++ packages/react-app/src/truncateAddress.ts
@@ -2,19 +2,25 @@
 
 const truncateRegex = /^(0x[a-zA-Z0-9]{5})[a-zA-Z0-9]+([a-zA-Z0-9]{5})$/;
 
+const truncateVeryShortRegex = /^(0x[a-zA-Z0-9]{3})[a-zA-Z0-9]+([a-zA-Z0-9]{3})$/;
+
+export const truncateEthAddressVeryShort = truncateEthAddressInternal.bind(null, truncateVeryShortRegex);
+
 // truncateEthAddress truncates the passed Ethereum address.
-export const truncateEthAddress = (address?: string, separator: string = '••••') => {
+export const truncateEthAddress = truncateEthAddressInternal.bind(null, truncateRegex);
+
+function truncateEthAddressInternal(regex: RegExp, address?: string, separator: string = '••••'): string {
   if (!address) return '';
   else {
-    const match = address.match(truncateRegex);
+    const match = address.match(regex);
     if (!match) return address;
     else return `${match[1]}${separator}${match[2]}`;
   }
-};
+}
 
 // truncateENSAddress truncates the passed ENS name to be no longer than
 // the passed maxLength.
-export const truncateENSAddress = (ensName: string, maxLength: number) => {
+export const truncateENSAddress = (ensName: string, maxLength: number): string => {
   if (ensName.length > maxLength) {
     return ensName.replace('.eth', '').slice(0, maxLength) + '...';
   } else {
