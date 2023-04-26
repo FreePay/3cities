7f3abd66cf98fb61b86443b0a7489be27d3aaf77
Ryan Berckmans <ryanberckmans@gmail.com>
2023-04-26 16:13:07 -0500

    Add blockExplorerUrls.ts

---
+++ packages/react-app/src/blockExplorerUrls.ts
@@ -0,0 +1,28 @@
+import { Chain } from "wagmi";
+import { getChain } from "./chains";
+
+export function getBlockExplorerUrlForTransaction(chainId: number | undefined, transactionHash: string): string | undefined {
+  const chain: Chain | undefined = getChain(chainId);
+  const blockExplorerUrl: string | undefined = getBlockExplorerUrl(chain);
+  if (blockExplorerUrl) {
+    return `${ensureUrlEndsWithSlash(blockExplorerUrl)}tx/${transactionHash}`;
+  } else return undefined;
+}
+
+export function getBlockExplorerUrlForAddress(chainId: number | undefined, address: string): string | undefined {
+  const chain: Chain | undefined = getChain(chainId);
+  const blockExplorerUrl: string | undefined = getBlockExplorerUrl(chain);
+  if (blockExplorerUrl) {
+    return `${ensureUrlEndsWithSlash(blockExplorerUrl)}address/${address}`;
+  } else return undefined;
+}
+
+function getBlockExplorerUrl(chain: Chain | undefined): string | undefined {
+  const blockExplorerUrl: string | undefined = chain?.blockExplorers?.default.url;
+  return blockExplorerUrl;
+}
+
+function ensureUrlEndsWithSlash(url: string): string {
+  if (url.endsWith('/')) return url;
+  else return url + '/';
+}
