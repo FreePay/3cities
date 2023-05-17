b00b2dfd7136bc8f5a376a3eb63e9b6ad57f2241
Ryan Berckmans <ryanberckmans@gmail.com>
2023-05-17 14:20:17 -0500

    Polish MainWrapper header/footer
    - remove nav icons on desktop
    - put twitter in bottom right of footer
    - fix onhover feedback for header nav links
    - fix responsive spacing

--- packages/react-app/src/MainWrapper.tsx
+++ packages/react-app/src/MainWrapper.tsx
@@ -29,27 +29,24 @@ const OurNavLink: React.FC<NavLinkProps & {
 const Header: React.FC = () => {
   return (
     <header className={`hidden sm:flex bg-quaternary p-5 min-h-[80px] items-center w-full ${headerFooterFont}`}>
-      <div className="flex flex-none items-center justify-start w-48">
+      <div className="flex flex-none items-center justify-start w-32 lg:w-48">
         <OurNavLink to="/">
           <Wordmark />
         </OurNavLink>
       </div>
-      <div className="flex flex-1 gap-16 items-center justify-center px-4">
-        <OurNavLink to="/" className="flex flex-col items-center text-center">
-          <FaHome />
+      <div className="flex flex-1 gap-8 lg:gap-16 items-center justify-center px-4">
+        <OurNavLink to="/" className="flex flex-col items-center text-center rounded-md px-2.5 py-1.5 transition sm:hover:bg-black sm:hover:bg-opacity-10">
           <span className="text-sm">Home</span>
         </OurNavLink>
-        <OurNavLink to="/request-money" className="flex flex-col items-center text-center">
-          <FaHandHoldingUsd />
+        <OurNavLink to="/request-money" className="flex flex-col items-center text-center rounded-md px-2.5 py-1.5 transition sm:hover:bg-black sm:hover:bg-opacity-10">
           <span className="text-sm">Request Money</span>
         </OurNavLink>
-        <OurNavLink to="/me" className="flex flex-col items-center text-center w-10">
-          <FaUserCircle />
+        <OurNavLink to="/me" className="flex flex-col items-center text-center w-10 rounded-md px-2.5 py-1.5 transition sm:hover:bg-black sm:hover:bg-opacity-10">
           <span className="text-sm">Me</span>
         </OurNavLink>
       </div>
       <div className="flex flex-none items-center justify-end">
-        <div className="max-sm:min-w-[46vw] max-sm:max-w-48 sm:w-48">
+        <div className="max-sm:min-w-[46vw] max-sm:max-w-32 lg:w-48">
           <ConnectWalletButtonCustom
             disconnectedLabel="Connect Wallet"
             className="rounded-md px-3.5 py-2 text-xs font-medium bg-white sm:enabled:hover:bg-gray-200 focus:outline-none active:scale-95 w-full"
@@ -67,7 +64,7 @@ const Footer: React.FC = () => {
   const hideFooterOnMobile = useContext(HideFooterOnMobileContext);
   return (
     <footer className={`fixed inset-x-0 bottom-0 sm:static bg-quaternary p-5 min-h-[80px] flex items-center ${headerFooterFont} ${hideFooterOnMobile ? 'max-sm:hidden' : ''}`}>
-      <div className="mx-auto flex w-full sm:w-auto sm:gap-16 items-center justify-between">
+      <div className="mx-auto flex w-full sm:w-auto sm:gap-8 lg:gap-16 items-center justify-between">
         <OurNavLink to="/" className="sm:hidden flex flex-col items-center text-center w-10">
           <FaHome />
           <span className="text-sm">Home</span>
@@ -80,15 +77,15 @@ const Footer: React.FC = () => {
           <FaUserCircle />
           <span className="text-sm">Me</span>
         </OurNavLink>
-        <OurNavLink to="/about" className="hidden sm:flex flex-none w-12 flex-col items-center text-center gap-1 rounded-md px-2.5 py-1.5 transition sm:hover:bg-black sm:hover:bg-opacity-10 justify-center">
+        <OurNavLink to="/faq" className="hidden sm:flex flex-none w-12 flex-col items-center text-center gap-1 rounded-md px-2.5 py-1.5 transition sm:hover:bg-black sm:hover:bg-opacity-10 justify-center">
+          <span className="text-sm">FAQ</span>
+        </OurNavLink>
+        <OurNavLink to="/about" className="hidden sm:flex flex-none flex-col items-center text-center gap-1 rounded-md px-2.5 py-1.5 transition sm:hover:bg-black sm:hover:bg-opacity-10 justify-center">
           <span className="text-sm">About</span>
         </OurNavLink>
         <a href="https://twitter.com/3cities_xyz" target="_blank" rel="noreferrer" className="hidden sm:flex flex-none w-12 items-center gap-1 rounded-md px-2.5 py-1.5 transition sm:hover:bg-black sm:hover:bg-opacity-10 justify-center">
           <FaTwitter />
         </a>
-        <OurNavLink to="/faq" className="hidden sm:flex flex-none w-12 flex-col items-center text-center gap-1 rounded-md px-2.5 py-1.5 transition sm:hover:bg-black sm:hover:bg-opacity-10 justify-center">
-          <span className="text-sm">FAQ</span>
-        </OurNavLink>
       </div>
     </footer>
   );
@@ -105,7 +102,7 @@ export const MainWrapper = () => {
     <div className="min-h-screen flex flex-col text-black">
       {/* <div className="fixed left-1/2 top-0 w-0.5 bg-red-500 h-full">this is a horizontally-centered vertical line for debug purposes</div> */}
       <Header />
-      <div className={`absolute top-6 right-5 text-sm text-quaternary-darker sm:hidden ${includeBackButtonOnMobile ? '' : 'hidden'}`} onClick={goBack}>back</div>
+      <div className={`absolute top-6 right-5 p-2 text-sm text-quaternary-darker sm:hidden ${includeBackButtonOnMobile ? '' : 'hidden'}`} onClick={goBack}>back</div>
       <div className="grow flex flex-col items-center justify-start bg-gray-100 pb-[120px] sm:pb-8">
         {/* NB the pb-[120px] on this parent div ensures there is sufficient (80px would be the minimum, and we added another 40px) blank space below the outlet that's the same height as the footer, which has the effect of ensuring that the user can scroll down to view the bottom of the content, otherwise the content bottom would be hidden behind the footer. */}
         <div className="w-full overflow-hidden px-5">
