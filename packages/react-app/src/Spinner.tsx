11460259b0893c54c2daf4331a3fe5c668452d04
Ryan Berckmans <ryanberckmans@gmail.com>
2023-09-25 15:48:08 -0500

    Add Spinner.speed

--- packages/react-app/src/Spinner.tsx
+++ packages/react-app/src/Spinner.tsx
@@ -3,12 +3,13 @@ import React from "react";
 type SpinnerProps = {
   containerClassName?: string; // className to apply to the spinner's container. Useful to eg. position the spinner
   spinnerClassName?: string; // className to apply to the spinner itself. The spinner color is the text color in this className (or inherited)
+  speed?: "slow" | "fast"; // defaults to "fast"
 }
 
-export function Spinner({ containerClassName, spinnerClassName }: SpinnerProps) {
+export function Spinner({ containerClassName, spinnerClassName, speed }: SpinnerProps) {
   // great collection of svg spinners: https://github.com/n3r4zzurr0/svg-spinners
   return <span className={containerClassName}>
-    <svg role="status" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={`${spinnerClassName || ''} animate-spin-fast`}>
+    <svg role="status" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={`${spinnerClassName || ''} ${speed === "slow" ? 'animate-spin' : 'animate-spin-fast'}`}>
       <path d="M10.72,19.9a8,8,0,0,1-6.5-9.79A7.77,7.77,0,0,1,10.4,4.16a8,8,0,0,1,9.49,6.52A1.54,1.54,0,0,0,21.38,12h.13a1.37,1.37,0,0,0,1.38-1.54,11,11,0,1,0-12.7,12.39A1.54,1.54,0,0,0,12,21.34h0A1.47,1.47,0,0,0,10.72,19.9Z" fill="currentColor" />
       {/* <path d="M2,12A11.2,11.2,0,0,1,13,1.05C12.67,1,12.34,1,12,1a11,11,0,0,0,0,22c.34,0,.67,0,1-.05C6,23,2,17.74,2,12Z" fill="currentColor" /> // this is an alternative style https://raw.githubusercontent.com/n3r4zzurr0/svg-spinners/main/svg-css/eclipse.svg */}
     </svg>
