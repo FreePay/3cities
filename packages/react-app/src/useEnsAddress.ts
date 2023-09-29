6ad258415d4e26757db27a9c2c602e458efc3315
Ryan Berckmans <ryanberckmans@gmail.com>
2023-09-29 14:29:13 -0500

    Rewrite useEnsName and useEnsAddress
    State machines now correctly handle all states.
    
    ENS name and address are now frequently refetched while page is visible.
    
    useEnsName now automatically performs forward resolution to verify
    insecure reverse resolution.

--- packages/react-app/src/useEnsAddress.ts
+++ packages/react-app/src/useEnsAddress.ts
@@ -1,12 +1,19 @@
+import { isAddress } from "@ethersproject/address";
 import { useMemo } from "react";
 import { goerli, mainnet, useEnsAddress as wagmiUseEnsAddress } from 'wagmi';
 import { isProduction } from "./isProduction";
+import { useIsPageVisibleOrRecentlyVisible } from "./useIsPageVisibleOrRecentlyVisible";
 
 // useEnsAddress is our higher-level wrapper around wagmi.useEnsAddress.
-// useEnsAddress returns the passed ENS name's primary address or
-// undefined if no primary address is set.
-export function useEnsAddress(ensName: string | undefined): {
-  address: `0x${string}`;
+// useEnsAddress returns the passed ENS name's address or `address:
+// undefined` if no address is set or the passed ensName is invalid.
+export function useEnsAddress(ensName: undefined): {
+  address?: never;
+  error?: never;
+  isLoading: false;
+}
+export function useEnsAddress(ensName: string): {
+  address: `0x${string}` | undefined;
   error?: never;
   isLoading: false;
 } | {
@@ -17,28 +24,69 @@ export function useEnsAddress(ensName: string | undefined): {
   address?: never;
   error?: never;
   isLoading: true;
+}
+export function useEnsAddress(ensName: string | undefined): {
+  address?: never;
+  error?: never;
+  isLoading: false;
+} | {
+  address: `0x${string}` | undefined;
+  error?: never;
+  isLoading: false;
+} | {
+  address?: never;
+  error: Error;
+  isLoading: false;
 } | {
   address?: never;
   error?: never;
-  isLoading?: never;
+  isLoading: true;
+}
+export function useEnsAddress(ensName: string | undefined): {
+  address?: never;
+  error?: never;
+  isLoading: false;
+} | {
+  address: `0x${string}` | undefined;
+  error?: never;
+  isLoading: false;
+} | {
+  address?: never;
+  error: Error;
+  isLoading: false;
+} | {
+  address?: never;
+  error?: never;
+  isLoading: true;
 } {
+  const isPageVisibleOrRecentlyVisible = useIsPageVisibleOrRecentlyVisible();
+  const isEnabled = isPageVisibleOrRecentlyVisible && Boolean(ensName && ensName.length > 0 && !isAddress(ensName)); // NB wagmi returns the cached result while disabled, so setting isEnabled==false while page is invisible does not cause the result to be undefined
   const args = useMemo(() => {
     return {
       chainId: isProduction ? mainnet.id : goerli.id,
       name: ensName ?? '',
-      enabled: Boolean(ensName && ensName.length > 1 && !ensName.startsWith('0x')),
+      enabled: isEnabled,
+      staleTime: 15_000, // milliseconds until cached result is considered stale and will be refetched if subsequently requested. If a user is temporarily offline, a result fetched while offline will be undefined and that undefined result will persist even after the user goes back online, so we mark it as stale to correctly fetch the actual result when back online.
     }
-  }, [ensName]);
+  }, [ensName, isEnabled]);
 
   const { data: address, error, isLoading } = wagmiUseEnsAddress(args);
 
-  if (isLoading) return {
-    isLoading: true,
-  }; else if (typeof address === 'string') return {
-    address,
+  if (ensName === undefined || ensName.length < 1) return {
     isLoading: false,
   }; else if (error) return {
     error,
     isLoading: false,
-  }; else return {};
+  }; else if (isLoading) return {
+    isLoading: true,
+  }; else if (address) return {
+    address,
+    isLoading: false,
+  }; else {
+    // console.log(`useEnsAddress returned undefined. Was it because the passed ensName had no address? ${JSON.stringify({ address, ensName, error, isLoading }, (_key, value) => typeof value === 'undefined' ? null : value)}`);
+    return {
+      address: undefined,
+      isLoading: false,
+    };
+  }
 }
--- packages/react-app/src/useEnsName.ts
+++ packages/react-app/src/useEnsName.ts
@@ -1,40 +1,111 @@
+import { isAddress } from "@ethersproject/address";
 import { useMemo } from "react";
 import { goerli, mainnet, useEnsName as wagmiUseEnsName } from 'wagmi';
 import { isProduction } from "./isProduction";
 import { makeAddress } from "./makeAddress";
 import { truncateENSAddress } from "./truncateAddress";
+import { useEnsAddress } from "./useEnsAddress";
+import { useIsPageVisibleOrRecentlyVisible } from "./useIsPageVisibleOrRecentlyVisible";
+
+type Opts = {
+  truncate?: boolean; // iff true, the returned ENS name will be truncated.
+  maxENSNameLength?: number; // iff truncated is true, the returned ENS name will be truncated to this length. Default 14.
+  unsafeDisableForwardResolutionVerification?: boolean; // iff true, disable automatic verification that the forward resolution of the returned ENS name resolves back to the passed address. WARNING this is unsafe because ENS does not enforce the accuracy of reverse records. To be sure a given primary name (reverse record) for an address is correct, we must perform a forward resolution back to the address (which is what useEnsName does by default unless this flag is set). Ie. "0x123" --> "foo.eth" [insecure reverse resolution to primary name] -> forward resolves back to "0x123" [now foo.eth is known to be the a secure primary name (reverse resolution) of 0x123].
+}
 
 // useEnsName is our higher-level wrapper around wagmi.useEnsName.
-// useEnsName returns the passed address's reverse record or undefined
-// if no reverse record is set, and truncates the returned ENS name if
-// the passed truncated option is set. WARNING ENS does not enforce the
-// accuracy of reverse records. To be sure a given name for an address
-// is correct, you must perform a forward resolution back to the
-// address. (Ie. "0x123" --> "foo.eth" [insecure reverse resolution] ->
-// resolves back to "0x123" [now foo.eth is known to be a secure reverse
-// resolution])
-export function useEnsName(address: undefined, opts?: {
-  truncated?: boolean;
-  maxENSNameLength?: number;
-}): undefined;
-export function useEnsName(address: string | undefined, opts?: {
-  truncated?: boolean;
-  maxENSNameLength?: number;
-}): string | undefined;
-export function useEnsName(address: string | undefined, opts?: {
-  truncated?: boolean; // iff true, the returned ENS name will be truncated
-  maxENSNameLength?: number; // iff truncated is true, the returned ENS name will be truncated to this length. Default 14
-}): string | undefined {
+// useEnsName returns the passed address's verified primary name
+// (reverse record) or `ensName: undefined` if no primary name is set,
+// primary name was set but verification failed (see
+// opts.unsafeDisableForwardResolutionVerification), or the passed
+// address is invalid.
+export function useEnsName(address: undefined, opts?: Opts): {
+  ensName?: never;
+  error?: never;
+  isLoading: false;
+}
+export function useEnsName(address: string, opts?: Opts): {
+  ensName: string | undefined;
+  error?: never;
+  isLoading: false;
+} | {
+  ensName?: never;
+  error: Error;
+  isLoading: false;
+} | {
+  ensName?: never;
+  error?: never;
+  isLoading: true;
+}
+export function useEnsName(address: string | undefined, opts?: Opts): {
+  ensName?: never;
+  error?: never;
+  isLoading: false;
+} | {
+  ensName: string | undefined;
+  error?: never;
+  isLoading: false;
+} | {
+  ensName?: never;
+  error: Error;
+  isLoading: false;
+} | {
+  ensName?: never;
+  error?: never;
+  isLoading: true;
+}
+export function useEnsName(address: string | undefined, opts?: Opts): {
+  ensName?: never;
+  error?: never;
+  isLoading: false;
+} | {
+  ensName: string | undefined;
+  error?: never;
+  isLoading: false;
+} | {
+  ensName?: never;
+  error: Error;
+  isLoading: false;
+} | {
+  ensName?: never;
+  error?: never;
+  isLoading: true;
+} {
+  const isPageVisibleOrRecentlyVisible = useIsPageVisibleOrRecentlyVisible();
+  const isEnabled = isPageVisibleOrRecentlyVisible && Boolean(address && isAddress(address)); // NB wagmi returns the cached result while disabled, so setting isEnabled==false while page is invisible does not cause the result to be undefined
   const args = useMemo(() => {
     return {
       chainId: isProduction ? mainnet.id : goerli.id,
       address: makeAddress(address || '0x01'), // WARNING HACK TODO permanent solution for makeAddress
-      enabled: address !== undefined,
+      enabled: isEnabled,
+      staleTime: 15_000, // milliseconds until cached result is considered stale and will be refetched if subsequently requested. If a user is temporarily offline, a result fetched while offline will be undefined and that undefined result will persist even after the user goes back online, so we mark it as stale to correctly fetch the actual result when back online.
     };
-  }, [address]);
-  const { data: ensName } = wagmiUseEnsName(args);
-  if (address !== undefined && typeof ensName === 'string') {
-    if (opts?.truncated) return truncateENSAddress(ensName, opts.maxENSNameLength ?? 14);
-    else return ensName;
-  } else return undefined;
+  }, [address, isEnabled]);
+
+  const { data: ensName, error: wagmiUseEnsNameError, isLoading: wagmiUseEnsNameIsLoading } = wagmiUseEnsName(args);
+
+  const { address: addressFromENSName, error: useEnsAddressError, isLoading: useEnsAddressIsLoading } = useEnsAddress((opts?.unsafeDisableForwardResolutionVerification ? undefined : ensName) ?? undefined);
+
+  const verificationSuccessful = Boolean(ensName && !useEnsAddressIsLoading && addressFromENSName && addressFromENSName.toLowerCase() === address?.toLowerCase()); // true iff forward resolution of the reverse record ENS name was found to have resolved back to the passed address, indicating the ensName for the passed address has been securely verified.
+
+  if (address === undefined || !isAddress(address)) return {
+    isLoading: false,
+  }; else if (wagmiUseEnsNameError) return {
+    error: wagmiUseEnsNameError,
+    isLoading: false,
+  }; else if (!opts?.unsafeDisableForwardResolutionVerification && useEnsAddressError) return {
+    error: new Error(`useEnsName: forward resolution verification error`, { cause: useEnsAddressError }),
+    isLoading: false,
+  }; else if (typeof ensName === 'string' && ensName.length > 0 && (opts?.unsafeDisableForwardResolutionVerification || verificationSuccessful)) return {
+    ensName: opts?.truncate ? truncateENSAddress(ensName, opts.maxENSNameLength ?? 14) : ensName,
+    isLoading: false,
+  }; else if (wagmiUseEnsNameIsLoading || (!opts?.unsafeDisableForwardResolutionVerification && useEnsAddressIsLoading)) return {
+    isLoading: true,
+  }; else {
+    // console.log(`useEnsName returned undefined. Was it because the passed address had no ens name xor verification failed? ${JSON.stringify({ address, ensName, wagmiUseEnsNameError, isLoading: wagmiUseEnsNameIsLoading, verificationSuccessful, addressFromENSName, useEnsAddressError, useEnsAddressIsLoading, opts }, (_key, value) => typeof value === 'undefined' ? null : value)}`);
+    return {
+      ensName: undefined,
+      isLoading: false,
+    };
+  }
 }
