190d3a295ee17a77d3234e4f8d0ed83dcc52391d
Ryan Berckmans <ryanberckmans@gmail.com>
2023-10-11 12:27:33 -0500

    Add flatMap

---
+++ packages/react-app/src/flatMap.ts
@@ -0,0 +1,11 @@
+
+// flatMap is a functional flatMap. We provide it because
+// Array.prototype.flatMap is designed to flatten nested arrays and is
+// not a true functional flatMap.
+export function flatMap<T, U>(ts: T[], f: (t: T) => U | undefined): U[] {
+  return ts.reduce((acc: U[], t: T): U[] => {
+    const maybeU: U | undefined = f(t);
+    if (maybeU) acc.push(maybeU);
+    return acc;
+  }, []);
+}
