88231f4eede921df603928e6a2c09acbcc613361
Ryan Berckmans <ryanberckmans@gmail.com>
2023-10-20 14:46:12 -0500

    fixup

--- packages/react-app/src/shuffleArray.ts
+++ packages/react-app/src/shuffleArray.ts
@@ -5,7 +5,7 @@ export function shuffleArray<T>(array: T[]): T[] {
   let currentIndex = array.length, randomIndex;
 
   // While there remain elements to shuffle.
-  while (currentIndex != 0) {
+  while (currentIndex !== 0) {
 
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
