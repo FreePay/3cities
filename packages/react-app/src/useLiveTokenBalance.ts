0aa3a5595f42d87541ee55a610c998d85eb04b89
Ryan Berckmans <ryanberckmans@gmail.com>
2023-10-20 13:50:34 -0500

    rm hooks directory

--- packages/react-app/src/useLiveTokenBalance.ts
+++ packages/react-app/src/useLiveTokenBalance.ts
@@ -1,26 +1,27 @@
 import { BigNumber } from "@ethersproject/bignumber";
 import { useMemo } from "react";
 import { useBalance } from 'wagmi';
-import { useIsEnabledSmartRefresh } from "../useIsEnabledSmartRefresh";
+import { useIsEnabledSmartRefresh } from "./useIsEnabledSmartRefresh";
 
-// useLiveNativeCurrencyBalance is a React hook that returns a
-// live-reloaded ETH (or other native chain currency) balance for the
-// passed address and chainId.
-export function useLiveNativeCurrencyBalance(
-  address: `0x${string}`, // address whose native currency balance will be live-reloaded
-  chainId: number, // chainId whose native currency balance will be live-reloaded
+// useLiveTokenBalance is a React hook that returns a live-reloaded token
+// balance for the passed token contract address, address, and chainId.
+export function useLiveTokenBalance(
+  tokenContractAddress: `0x${string}`, // token contract address whose token balance will be live-reloaded
+  address: `0x${string}`, // address whose token balance will be live-reloaded
+  chainId: number, // chainId on which tokenAddress contract resides
 ): BigNumber | undefined {
   const enabled = useIsEnabledSmartRefresh();
   const args = useMemo(() => {
     return {
       enabled, // here, we are periodically toggling enabled to force a useBalance refresh. We explored solutions like `watch: true` and `staleTime/cacheTime`, and none of them worked for us. See 'redesign of live balance fetching' in 3cities design. Crucially, this toggle works because when useBalance is disabled, it continues returning the most recent result and doesn't return undefined, so the client is unaware that useBalance has been disabled.
+      token: tokenContractAddress,
       address,
       chainId,
       onError(error: Error) {
-        console.error('useLiveNativeCurrencyBalance: error fetching balance with wagmi.useBalance, chainId', chainId, 'address', address, 'error', error);
+        console.error('useLiveTokenBalance: error fetching balance with wagmi.useBalance, chainId', chainId, 'token', tokenContractAddress, 'address', address, 'error', error);
       },
-    }
-  }, [chainId, address, enabled]);
+    };
+  }, [chainId, address, tokenContractAddress, enabled]);
   const r = useBalance(args);
   const bRaw = r.data === undefined ? undefined : r.data.value;
   // eslint-disable-next-line react-hooks/exhaustive-deps
