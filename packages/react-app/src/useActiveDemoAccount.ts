795c4d62668d929cc46375654cdf34960d458a40
Ryan Berckmans <ryanberckmans@gmail.com>
2023-11-26 17:09:26 -0500

    Propagate observer to useActiveDemoAccount

--- packages/react-app/src/useActiveDemoAccount.ts
+++ packages/react-app/src/useActiveDemoAccount.ts
@@ -1,8 +1,10 @@
 import { useContext } from "react";
 import { ActiveDemoAccountContext } from "./ActiveDemoAccountContext";
+import { Observer, useObservedValue } from "./observer";
 
 // useActiveDemoAccount returns the active global demo account or
 // undefined if no demo account is active.
 export function useActiveDemoAccount(): string | undefined {
-  return useContext(ActiveDemoAccountContext);
+  const o: Observer<string | undefined> = useContext(ActiveDemoAccountContext);
+  return useObservedValue(o);
 }
