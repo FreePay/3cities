145b1673de76b07afad95d2fac3f4df38a9abc6c
Ryan Berckmans <ryanberckmans@gmail.com>
2024-07-08 16:25:48 -0500

    fixup

--- packages/react-app/src/useMemoObject.ts
+++ packages/react-app/src/useMemoObject.ts
@@ -3,7 +3,6 @@ import { useMemo } from 'react';
 // TODO doc
 export function useMemoObject<O extends object, K extends keyof O>(o: O, keys: K[]): O {
   return useMemo(() => {
-    console.log("ry useMemoObject recache", o, keys); // TODO rm
     return o;
     // eslint-disable-next-line react-hooks/exhaustive-deps -- here by definition we want to update the memoized object iff any of the passed keys' values have changed
   }, keys.map(key => o[key]));
@@ -14,7 +13,6 @@ export function useMemoObject<O extends object, K extends keyof O>(o: O, keys: K
 // where the subset of fields is identified by the passed keys.
 export function useMemoObjectOld<O extends object, K extends keyof O>(o: O, keys: K[]): Pick<O, K> {
   return useMemo(() => {
-    console.log("ry useMemoObject recache", o, keys); // TODO rm
     return { ...o }; // NB here the memoized object includes all fields in o, not just the passed keys. However, this allows us to construct the memoized value in a typesafe way, and there doesn't seem to be much downside to including all fields in the memoized object. The recomputation happens iff a field changes for a passed key.
     // eslint-disable-next-line react-hooks/exhaustive-deps -- here we want to update the memoized object iff any values have changed
   }, keys.map(key => o[key]));
